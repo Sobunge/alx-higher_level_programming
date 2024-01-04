d64c41879745e5ef2a4268c6e9a1fda6dfe29943
Samuel Obunge <samuelobunge@yahoo.com>
2024-01-04 16:46:38 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,6 +3,8 @@
 nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
+
+
 from sys import argv
 if __name__ == "__main__":
     a = []
