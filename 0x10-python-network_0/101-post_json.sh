d99d880c4a5db98c38a2d10bd8d46216e39c7643
Samuel Obunge <samuelobunge@yahoo.com>
2024-03-28 00:54:44 +0300

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a given URL with a given JSON file.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+
+curl -s -X GET -H "X-School-User-Id: 98" "$1"
