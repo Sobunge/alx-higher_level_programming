cb0824280cea415c1c926ba7c6aadc63fa9f16f6
Samuel Obunge <samuelobunge@yahoo.com>
2024-03-28 14:10:55 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-This script fetches the status of https://alx-intranet.hbtn.io/status
-using the urllib package.
-"""
-import urllib.request
 
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
+if __name__ == '__main__':
+    import urllib.request
 
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
