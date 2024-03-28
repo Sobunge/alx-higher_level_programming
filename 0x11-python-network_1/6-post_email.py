90a69911e19775b9db39298dc2c18e1a690587c7
samuel obunge <samuelobunge@gmail.com>
2024-03-28 04:18:55 -0700

    commit

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-This script takes in a URL and an email address, sends a POST request to the
-passed URL with the email as a parameter, and displays the body of the response.
+This script takes in a URL and an email address,
+sends a POST request to the
+passed URL with the email as a parameter,
+and displays the body of the response.
 """
 import requests
 import sys
